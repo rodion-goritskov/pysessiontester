edb44db43f0eb920d952f10631b466d28c4ef180
Rodion Goritskov <rodion.goritskov@gmail.com>
2013-12-23 16:16:16 +0400

    Line end symbol execution during export-to-html

--- export_session.py
+++ export_session.py
@@ -4,6 +4,7 @@ import datetime
 def export_session_to_html(text_field):
     '''Exports everything from text_field to html file
     with current session day and time'''
+    res = ""
     session_date = datetime.datetime.today()
     session_date_string = session_date.strftime('%d %b %Y %X')
     with open('Session at ' + session_date_string + '.html', mode='w',
@@ -11,4 +12,9 @@ def export_session_to_html(text_field):
         export_file.write(
             '<meta http-equiv="Content-Type" content="text/html;charset=UTF-8">\n')
         export_file.write('<h1>Session at ' + session_date_string + '</h1>\n')
-        export_file.write('<p>' + text_field + '</p>')
+        for i in text_field:
+            if i == "\n":
+                res = res + "</br>"
+            else:
+                res = res + i
+        export_file.write('<p>' + res + '</p>')
