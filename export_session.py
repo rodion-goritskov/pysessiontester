2dc3012dfed7cc40f16bb2c3ceeae0ae9ac13bbb
Rodion Goritskov <rodion.goritskov@gmail.com>
2014-01-27 23:19:45 +0400

    Added EXPORT_PATH hardcoded variable

--- export_session.py
+++ export_session.py
@@ -1,4 +1,7 @@
 import datetime
+import os
+
+EXPORT_PATH = os.environ["HOME"] + "/Документы/Проекты/Izumrud/Тестовые сессии/"
 
 
 def export_session_to_html(text_field):
@@ -8,7 +11,8 @@ def export_session_to_html(text_field):
     pair = False
     session_date = datetime.datetime.today()
     session_date_string = session_date.strftime('%d %b %Y %X')
-    with open('Session at ' + session_date_string + '.html', mode='w',
+    with open(EXPORT_PATH + 'Session at ' + session_date_string + '.html',
+              mode='w',
               encoding='utf-8') as export_file:
         export_file.write(
             '<meta http-equiv="Content-Type" content="text/html;charset=UTF-8">\n')
