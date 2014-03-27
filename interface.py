cfa66687d6c94739d7101daa167992d3087b0189
Rodion Goritskov <rodion.goritskov@gmail.com>
2014-03-27 23:25:40 +0400

    Some more interface layout changes

--- interface.py
+++ interface.py
@@ -2,7 +2,7 @@
 
 # Form implementation generated from reading ui file 'untitled.ui'
 #
-# Created: Thu Mar 27 23:12:35 2014
+# Created: Thu Mar 27 23:24:40 2014
 #      by: PyQt4 UI code generator 4.10.4
 #
 # WARNING! All changes made in this file will be lost!
@@ -27,7 +27,8 @@ class Ui_MainAppWindow(object):
     def setupUi(self, MainAppWindow):
         MainAppWindow.setObjectName(_fromUtf8("MainAppWindow"))
         MainAppWindow.setWindowModality(QtCore.Qt.ApplicationModal)
-        MainAppWindow.resize(800, 593)
+        MainAppWindow.resize(800, 600)
+        MainAppWindow.setMinimumSize(QtCore.QSize(800, 600))
         MainAppWindow.setMaximumSize(QtCore.QSize(800, 600))
         icon = QtGui.QIcon()
         icon.addPixmap(QtGui.QPixmap(_fromUtf8("trayicon.xpm")), QtGui.QIcon.Normal, QtGui.QIcon.Off)
@@ -35,7 +36,7 @@ class Ui_MainAppWindow(object):
         self.centralwidget = QtGui.QWidget(MainAppWindow)
         self.centralwidget.setObjectName(_fromUtf8("centralwidget"))
         self.horizontalLayoutWidget = QtGui.QWidget(self.centralwidget)
-        self.horizontalLayoutWidget.setGeometry(QtCore.QRect(0, 0, 801, 31))
+        self.horizontalLayoutWidget.setGeometry(QtCore.QRect(10, 0, 781, 31))
         self.horizontalLayoutWidget.setObjectName(_fromUtf8("horizontalLayoutWidget"))
         self.horizontalLayout = QtGui.QHBoxLayout(self.horizontalLayoutWidget)
         self.horizontalLayout.setMargin(0)
@@ -46,13 +47,17 @@ class Ui_MainAppWindow(object):
         self.exportSessionButton = QtGui.QPushButton(self.horizontalLayoutWidget)
         self.exportSessionButton.setObjectName(_fromUtf8("exportSessionButton"))
         self.horizontalLayout.addWidget(self.exportSessionButton)
-        spacerItem = QtGui.QSpacerItem(40, 20, QtGui.QSizePolicy.Expanding, QtGui.QSizePolicy.Minimum)
-        self.horizontalLayout.addItem(spacerItem)
+        self.pushButton_3 = QtGui.QPushButton(self.horizontalLayoutWidget)
+        self.pushButton_3.setObjectName(_fromUtf8("pushButton_3"))
+        self.horizontalLayout.addWidget(self.pushButton_3)
+        self.pushButton_4 = QtGui.QPushButton(self.horizontalLayoutWidget)
+        self.pushButton_4.setObjectName(_fromUtf8("pushButton_4"))
+        self.horizontalLayout.addWidget(self.pushButton_4)
         self.sessionTimeField = QtGui.QTimeEdit(self.horizontalLayoutWidget)
         self.sessionTimeField.setObjectName(_fromUtf8("sessionTimeField"))
         self.horizontalLayout.addWidget(self.sessionTimeField)
         self.horizontalLayoutWidget_2 = QtGui.QWidget(self.centralwidget)
-        self.horizontalLayoutWidget_2.setGeometry(QtCore.QRect(-1, 29, 801, 541))
+        self.horizontalLayoutWidget_2.setGeometry(QtCore.QRect(9, 29, 781, 541))
         self.horizontalLayoutWidget_2.setObjectName(_fromUtf8("horizontalLayoutWidget_2"))
         self.horizontalLayout_2 = QtGui.QHBoxLayout(self.horizontalLayoutWidget_2)
         self.horizontalLayout_2.setMargin(0)
@@ -60,19 +65,8 @@ class Ui_MainAppWindow(object):
         self.textEdit = QtGui.QTextEdit(self.horizontalLayoutWidget_2)
         self.textEdit.setObjectName(_fromUtf8("textEdit"))
         self.horizontalLayout_2.addWidget(self.textEdit)
-        self.verticalLayout = QtGui.QVBoxLayout()
-        self.verticalLayout.setObjectName(_fromUtf8("verticalLayout"))
-        spacerItem1 = QtGui.QSpacerItem(20, 40, QtGui.QSizePolicy.Minimum, QtGui.QSizePolicy.Expanding)
-        self.verticalLayout.addItem(spacerItem1)
-        self.pushButton_3 = QtGui.QPushButton(self.horizontalLayoutWidget_2)
-        self.pushButton_3.setObjectName(_fromUtf8("pushButton_3"))
-        self.verticalLayout.addWidget(self.pushButton_3)
-        self.pushButton_4 = QtGui.QPushButton(self.horizontalLayoutWidget_2)
-        self.pushButton_4.setObjectName(_fromUtf8("pushButton_4"))
-        self.verticalLayout.addWidget(self.pushButton_4)
-        self.horizontalLayout_2.addLayout(self.verticalLayout)
         self.horizontalLayoutWidget_3 = QtGui.QWidget(self.centralwidget)
-        self.horizontalLayoutWidget_3.setGeometry(QtCore.QRect(0, 570, 801, 31))
+        self.horizontalLayoutWidget_3.setGeometry(QtCore.QRect(10, 570, 781, 31))
         self.horizontalLayoutWidget_3.setObjectName(_fromUtf8("horizontalLayoutWidget_3"))
         self.horizontalLayout_3 = QtGui.QHBoxLayout(self.horizontalLayoutWidget_3)
         self.horizontalLayout_3.setMargin(0)
@@ -93,8 +87,8 @@ class Ui_MainAppWindow(object):
         MainAppWindow.setWindowTitle(_translate("MainAppWindow", "PySessionTester", None))
         self.newSessionButton.setText(_translate("MainAppWindow", "New session", None))
         self.exportSessionButton.setText(_translate("MainAppWindow", "Export session", None))
+        self.pushButton_3.setText(_translate("MainAppWindow", "Settings", None))
+        self.pushButton_4.setText(_translate("MainAppWindow", "Take screenshot", None))
         self.sessionTimeField.setDisplayFormat(_translate("MainAppWindow", "hh:mm:ss", None))
-        self.pushButton_3.setText(_translate("MainAppWindow", "PushButton", None))
-        self.pushButton_4.setText(_translate("MainAppWindow", "PushButton", None))
         self.sessionStartButton.setText(_translate("MainAppWindow", "Start session", None))
 
